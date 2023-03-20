1b128f223f4cf18fddab0a984a89f886770cac68
wenzel audrin barboza <wenzelbarboza@gmail.com>
2023-03-20 19:47:16 +0200

    basic home page structure 1.0

---
+++ src/components/BdayItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BdayItem = () => {
+    return (
+        <div>BdayItem</div>
+    )
+}
+
+export default BdayItem
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import { atom, useAtom } from 'jotai'
+
+export const bdayList = atom([{
+    img: 'link',
+    name: 'alan',
+    age: '21'
+},
+{
+    img: 'link',
+    name: 'shawn',
+    age: '25'
+},
+{
+    img: 'link',
+    name: 'gwen',
+    age: '22'
+},
+{
+    img: 'link',
+    name: 'wilson',
+    age: '25'
+},
+])
 
 const Home = () => {
+
+    const [data, setData] = useAtom(bdayList)
+
     return (
-        <div>Home</div>
+        <div className='w-screen h-screen flex justify-center items-center'>
+            <div className='w-[28rem] h-[32rem] p-3 shadow-xl bg-blue-300 text-white rounded-md'>
+                {data.map((item, index) => {
+                    return (
+                        <>
+                            <h3>{item.img}</h3>
+                            <h3>{item.name}</h3>
+                            <h3>{item.age}</h3>
+                        </>
+                    )
+                })}
+            </div>
+        </div>
     )
 }
 
